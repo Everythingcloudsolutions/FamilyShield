3106816efe49162bce4749978b68b82f216495b4
Mohit Goyal <mohitkumargoyal@gmail.com>
2026-04-06 16:29:13 -0400

    fix: correct Terraform syntax - replace semicolons with newlines in variable definitions

--- iac/modules/cloudflare-dns/variables.tf
+++ iac/modules/cloudflare-dns/variables.tf
@@ -1,12 +1,47 @@
-variable "cloudflare_zone_id"    { type = string }
-variable "cloudflare_account_id" { type = string }
-variable "subdomain"             { type = string }
-variable "root_domain"           { type = string }
-variable "tunnel_secret"         { type = string; sensitive = true }
-variable "environment"           { type = string }
-variable "vm_public_ip"          { type = string }
-variable "admin_emails"          { type = list(string); default = [] }
+variable "cloudflare_zone_id" {
+  type = string
+}
 
-output "tunnel_id"    { value = cloudflare_tunnel.main.id; sensitive = true }
-output "tunnel_token" { value = cloudflare_tunnel.main.tunnel_token; sensitive = true }
-output "portal_url"   { value = "https://${var.subdomain}.${var.root_domain}" }
+variable "cloudflare_account_id" {
+  type = string
+}
+
+variable "subdomain" {
+  type = string
+}
+
+variable "root_domain" {
+  type = string
+}
+
+variable "tunnel_secret" {
+  type      = string
+  sensitive = true
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "vm_public_ip" {
+  type = string
+}
+
+variable "admin_emails" {
+  type    = list(string)
+  default = []
+}
+
+output "tunnel_id" {
+  value     = cloudflare_tunnel.main.id
+  sensitive = true
+}
+
+output "tunnel_token" {
+  value     = cloudflare_tunnel.main.tunnel_token
+  sensitive = true
+}
+
+output "portal_url" {
+  value = "https://${var.subdomain}.${var.root_domain}"
+}
--- iac/modules/supabase/variables.tf
+++ iac/modules/supabase/variables.tf
@@ -1,6 +1,20 @@
-variable "supabase_org_id"  { type = string }
-variable "environment"      { type = string }
-variable "db_password"      { type = string; sensitive = true }
+variable "supabase_org_id" {
+  type = string
+}
 
-output "project_id"  { value = supabase_project.familyshield.id }
-output "api_url"     { value = "https://${supabase_project.familyshield.id}.supabase.co" }
+variable "environment" {
+  type = string
+}
+
+variable "db_password" {
+  type      = string
+  sensitive = true
+}
+
+output "project_id" {
+  value = supabase_project.familyshield.id
+}
+
+output "api_url" {
+  value = "https://${supabase_project.familyshield.id}.supabase.co"
+}
