fb05fb28489acab7a95d8970196ad34aef79c39d
Mohit Goyal <mohitkumargoyal@gmail.com>
2026-04-06 16:25:13 -0400

    iac: set OCI ARM image OCID for ca-toronto-1

--- iac/variables.tf
+++ iac/variables.tf
@@ -32,7 +32,7 @@ variable "oci_ubuntu_arm_image_id" {
   type        = string
   # Find via: oci compute image list --compartment-id <tenancy-ocid> --operating-system "Canonical Ubuntu" --shape VM.Standard.A1.Flex
   # Current ca-toronto-1 Ubuntu 22.04 ARM image (update periodically):
-  default = "ocid1.image.oc1.ca-toronto-1.aaaaaaaawzbmdqqyvrctw4cvhegvnbbxtoday4bxlkdpqeowc5kcbrhplt2aocid1.image.oc1.ca-toronto-1.aaaaaaaaxxx" # Replace with actual OCID
+  default = "ocid1.image.oc1.ca-toronto-1.aaaaaaaawzbmdqqvrcLW4cvhegvnbbxtoday4bxlkdpqeowc5kcbrhplit2a"
 }
 
 variable "ssh_public_key" {
